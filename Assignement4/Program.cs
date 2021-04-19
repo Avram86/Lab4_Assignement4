93c50acb64fb9d2edafb074d734b51ce17ea0d0c
Avram86 <60349668+Avram86@users.noreply.github.com>
2021-04-19 11:26:17 +0300

    Add project files.

---
+++ Assignement4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Assignement4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assignement4();
+        }
+
+        public static void Assignement4()
+        {
+            Console.WriteLine("Please write a word:");
+            var input = Console.ReadLine();
+
+            bool isPalindrom = false;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = input.Length - 1; j >= 0; j--)
+                {
+                    if (input[i] == input[j])
+                    {
+                        isPalindrom = true;
+                        i++;
+                    }
+                    else
+                    {
+                        isPalindrom = false;
+                        break;
+                    }
+                }
+                break;
+            }
+
+            Console.WriteLine($"Cuvantul introdus este un palindrom: {isPalindrom}");
+        }
+
+    }
+}
